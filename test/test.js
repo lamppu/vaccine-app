6576af26d52e3974541a0efc27eb4a3634f33e89
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-06-08 11:06:31 +0300

    Setting up mocha for testing

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+const expect = require('chai').expect;
+
+describe('description of this set of tests', () => {
+  it('describe what the outcome should be', () => {
+    expect(1+1).to.equal(2);
+  });
+});
