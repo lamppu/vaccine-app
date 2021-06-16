80a225cdf1d86ac095fde45ab538686563cfe227
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-06-16 18:34:52 +0300

    Divided tests to separate files, added a local mysql client to knexfile

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -3,22 +3,38 @@ const getPath = require('../utils/get_path.js');
 
 let envPath = getPath('\\server\\config', '../../.env', './.env');
 let sqlitePath = getPath('\\server\\config', '../models/db.sqlite3', 'server/models/db.sqlite3');
+let migrationsPath = getPath('\\server\\config', '../models/migrations', 'server/models/migrations');
+let seedsPath = getPath('\\server\\config', '../models/seeds', 'server/models/seeds');
 
 dotenv.config({path: envPath});
 
 module.exports = {
-
+  test: {
+    client: 'mysql',
+    connection: {
+      host: process.env.L_MYSQL_HOST,
+      user: process.env.L_MYSQL_USER,
+      password: process.env.L_MYSQL_PWD,
+      database: process.env.L_MYSQL_DB,
+    },
+    migrations: {
+      directory: migrationsPath
+    },
+    seeds: {
+      directory: seedsPath
+    },
+    debug: false
+  },
   development: {
-
     client: 'sqlite3',
     connection: {
       filename: sqlitePath
     },
     migrations: {
-      directory: '../models/migrations'
+      directory: migrationsPath
     },
     seeds: {
-      directory: '../models/seeds'
+      directory: seedsPath
     },
     debug: false
   },
@@ -32,10 +48,10 @@ module.exports = {
       database: process.env.MYSQL_DB,
     },
     migrations: {
-      directory: '../models/migrations'
+      directory: migrationsPath
     },
     seeds: {
-      directory: '../models/seeds'
+      directory: seedsPath
     },
     debug: false
   }
---
+++ test/suites/local_mysql.js
@@ -0,0 +1,48 @@
+const expect = require('chai').expect;
+const knex = require('knex');
+const config = require('../../server/config/knexfile.js')['test'];
+const mysql = (knex)(config);
+const bookshelf = require('bookshelf')(mysql);
+
+const Order = bookshelf.model('Order', {
+  tableName: 'Order'
+});
+
+const Vaccination = bookshelf.model('Vaccination', {
+  tableName: 'Vaccination',
+  sourceBottle() {
+    return this.belongsTo('Order');
+  }
+})
+
+const getTables = async () => {
+  return await mysql('information_schema.TABLES')
+  .select('table_name')
+  .where('table_schema', process.env.L_MYSQL_DB)
+  .andWhereNot('table_name', 'knex_migrations')
+  .andWhereNot('table_name', 'knex_migrations_lock');
+}
+
+describe('Testing local mysql database', function() {
+  it('database should have two tables', async () => {
+    expect(await getTables()).to.have.lengthOf(2);
+  });
+
+  it('database should have tables "Order" and "Vaccination"', async () => {
+    const tables = await getTables();
+
+    const first = tables[0].TABLE_NAME || tables[0].table_name;
+    const second = tables[1].TABLE_NAME || tables[1].table_name;
+
+    expect(first.toLowerCase()).to.equal('order');
+    expect(second.toLowerCase()).to.equal('vaccination');
+  });
+
+  it('table "Order" in database should have 5000 rows', async () => {
+    expect(await Order.count()).to.equal(5000);
+  });
+
+  it('table "Vaccination" in database should have 7000 rows', async () => {
+    expect(await Vaccination.count()).to.equal(7000);
+  });
+});
