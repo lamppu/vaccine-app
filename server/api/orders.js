6217b0215ac3870ed7ddf14b6a49fbfe0a302904
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-07-19 23:38:34 +0300

    Added implementation for getting number of orders and vaccines per producer and per healthcare district

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,7 +2,8 @@ const Order = require('../models/bookshelf/order.js');
 const formatResponse = require('./utils/format_response.js');
 const getBeginTimestamp = require('./utils/begin_stamp.js');
 
-const allOrders = async (dateString) => {
+// The total number of orders and vaccines that have arrived on requested date, total & per producer & per district
+const ordersAndVaccines = async (dateString) => {
   if (!dateString) {
     return formatResponse(false, null, 400, "No date selected")
   }
@@ -14,9 +15,50 @@ const allOrders = async (dateString) => {
   const beginTS = getBeginTimestamp(endTS);
 
   const numOrders = await Order.whereBetween('arrived', beginTS, endTS).count();
+
+  const zerpfyOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'Zerpfy').count();
+  const antiquaOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'Antiqua').count();
+  const solarBuddhicaOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'SolarBuddhica').count();
+
+  const hyksOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'HYKS').count();
+  const kysOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'KYS').count();
+  const oysOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'OYS').count();
+  const taysOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'TAYS').count();
+  const tyksOrders = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'TYKS').count();
+
   const numVaccines = await Order.whereBetween('arrived', beginTS, endTS).query().sum({injections: 'injections'});
 
-  return  formatResponse(true, {"orders": numOrders, "vaccines": numVaccines[0].injections}, null, null);
+  const zerpfyVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'Zerpfy').query().sum({injections: 'injections'});
+  const antiquaVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'Antiqua').query().sum({injections: 'injections'});
+  const solarBuddhicaVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('vaccine', 'SolarBuddhica').query().sum({injections: 'injections'});
+
+  const hyksVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'HYKS').query().sum({injections: 'injections'});
+  const kysVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'KYS').query().sum({injections: 'injections'});
+  const oysVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'OYS').query().sum({injections: 'injections'});
+  const taysVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'TAYS').query().sum({injections: 'injections'});
+  const tyksVaccines = await Order.whereBetween('arrived', beginTS, endTS).andWhere('healthCareDistrict', 'TYKS').query().sum({injections: 'injections'});
+
+  let data = {
+    "orders": numOrders,
+    "zerpfyOrders": zerpfyOrders,
+    "antiquaOrders": antiquaOrders,
+    "solarBuddhicaOrders": solarBuddhicaOrders,
+    "hyksOrders": hyksOrders,
+    "kysOrders": kysOrders,
+    "oysOrders": oysOrders,
+    "taysOrders": taysOrders,
+    "tyksOrders": tyksOrders,
+    "vaccines": numVaccines[0].injections,
+    "zerpfyVaccines": zerpfyVaccines[0].injections,
+    "antiquaVaccines": antiquaVaccines[0].injections,
+    "solarBuddhicaVaccines": solarBuddhicaVaccines[0].injections,
+    "hyksVaccines": hyksVaccines[0].injections,
+    "kysVaccines": kysVaccines[0].injections,
+    "oysVaccines": oysVaccines[0].injections,
+    "taysVaccines": taysVaccines[0].injections,
+    "tyksVaccines": tyksVaccines[0].injections
+  };
+  return  formatResponse(true, data, null, null);
 }
 
-module.exports.allOrders = allOrders;
+module.exports.ordersAndVaccines = ordersAndVaccines;
