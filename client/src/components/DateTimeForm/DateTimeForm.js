5b3ee4916ee7d9e46c2804b0393017044f852305
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-07-22 17:18:14 +0300

    Added fetching data from server on submit

--- client/src/components/DateTimeForm/DateTimeForm.js
+++ client/src/components/DateTimeForm/DateTimeForm.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DateTimeForm.css';
-
-
 import { Form, Icon, Button, Popup } from 'semantic-ui-react';
 
-
-const DateTimeForm = ({onDateTimeStringChange, selectedDate, selectedTime, onSelectedDateChange, onSelectedTimeChange}) => {
+const DateTimeForm = ({onDateTimeStringChange, onOrdersAndVaccinesChange}) => {
+  const [selectedDate, setSelectedDate] = useState('2021-01-02');
+  const [selectedTime, setSelectedTime] = useState('');
   const handleDateChange = (e) => {
-    onSelectedDateChange(e.target.value);
+    setSelectedDate(e.target.value);
   }
   const handleTimeChange = (e) => {
-    onSelectedTimeChange(e.target.value);
+    setSelectedTime(e.target.value);
   }
-  //2021-01-02T16:25:03.693461Z
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const time = (selectedTime !== '') ? selectedTime : '23:59:59';
-    onDateTimeStringChange(selectedDate + 'T' + time + 'Z');
+    const dateString = selectedDate + 'T' + time + 'Z';
+    onDateTimeStringChange(dateString);
+
+    const ordersUrl = 'http://localhost:3001/orders?date=' + dateString;
+    let resp = await fetch(ordersUrl);
+
+    if (resp.status === 200) {
+      resp = await resp.json();
+      onOrdersAndVaccinesChange(resp.data);
+    }
+
   }
+
   return (
     <Form className='DateTimeForm'>
       <label>
@@ -50,5 +59,62 @@ const DateTimeForm = ({onDateTimeStringChange, selectedDate, selectedTime, onSel
     </Form>
   )
 }
+/*
+const DateTimeForm = ({onDateTimeStringChange, selectedDate, selectedTime, onSelectedDateChange, onSelectedTimeChange, onOrdersAndVaccinesChange}) => {
+  const handleDateChange = (e) => {
+    onSelectedDateChange(e.target.value);
+  }
+  const handleTimeChange = (e) => {
+    onSelectedTimeChange(e.target.value);
+  }
+  const handleSubmit = async () => {
+    const time = (selectedTime !== '') ? selectedTime : '23:59:59';
+    const dateString = selectedDate + 'T' + time + 'Z';
+    onDateTimeStringChange(dateString);
+
+    const ordersUrl = 'http://localhost:3001/orders?date=' + dateString;
+    let resp = await fetch(ordersUrl);
+
+    if (resp.status === 200) {
+      resp = await resp.json();
+      onOrdersAndVaccinesChange(resp.data);
+    }
+
+  }
+
+  return (
+    <Form className='DateTimeForm'>
+      <label>
+        Choose a date and time between January 2<sup>nd</sup> and
+        April 12<sup>th</sup> 2021
+        <Popup
+          trigger={<Icon name='info circle'/>}
+          content='Please note that this app uses mock data'
+          position='right center'
+        />
+      </label>
+      <Form.Group inline>
+        <Form.Input
+          label='Date'
+          type='date'
+          min='2021-01-02'
+          max='2021-04-12'
+          defaultValue={selectedDate}
+          onChange={handleDateChange}
+        />
+        <Form.Input
+          label="Time (optional)"
+          type='time'
+          step='1'
+          defaultValue={selectedTime}
+          onChange={handleTimeChange}
+        />
+        <Button type='submit' onClick={handleSubmit}>Show data for chosen date</Button>
+      </Form.Group>
+
+    </Form>
+  )
+}
+*/
 
 export default DateTimeForm;
