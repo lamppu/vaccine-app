087483265b56577e5edfd043a7203633a0e4ff86
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-07-25 18:39:48 +0300

    Fixed an error

--- server/api/utils/validate_date.js
+++ server/api/utils/validate_date.js
@@ -1,10 +1,10 @@
 const validateDate = (dateString) => {
-  if (!dateString) {
+  if (!dateString || dateString == '""') {
     return {"valid": false, "message": "No date selected"};
   }
   const d = new Date(dateString);
 
-  if (d === 'Invalid Date') {
+  if (d == 'Invalid Date') {
     return {"valid": false, "message": "Invalid Date"};
   }
   return {"valid": true, "message": null};
