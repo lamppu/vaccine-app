9a2301050d2fbb0ac2b6727f94a4139938247f3f
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2021-08-05 16:07:02 +0300

    Fixed getNewDate function

--- server/api/utils/get_date.js
+++ server/api/utils/get_date.js
@@ -1,5 +1,24 @@
 const getNewDate = (oldDate, days) => {
-  return new Date(oldDate.setDate(oldDate.getDate() + days));
+  const date = new Date(oldDate);
+  date.setDate(date.getDate() + days);
+
+  const oldOffset = oldDate.getTimezoneOffset();
+  const newOffset = date.getTimezoneOffset();
+
+  if (oldOffset === newOffset) {
+    return date;
+  }
+  let hours;
+  if (oldOffset > newOffset) {
+    hours = Math.abs(Math.abs(oldOffset) - Math.abs(newOffset))/60;
+    date.setHours(date.getHours() + hours);
+    return date;
+  }
+
+  hours = Math.abs(Math.abs(newOffset) - Math.abs(oldOffset))/60;
+  date.setHours(date.getHours() - hours);
+
+  return date;
 }
 
 module.exports = getNewDate;
