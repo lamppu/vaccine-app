8e72916aa52627aabb062fb91517e48c306f0e70
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2022-01-29 22:33:17 +0200

    Added count for arrived vaccines

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,12 +1,13 @@
 const queryOrders = require('./utils').queryOrders;
-const queryProducersAndIds = require('./utils').queryProducersAndIds;
+const queryVaccineRows = require('./utils').queryVaccineRows;
 const queryOrdersWithKey = require('./utils').queryOrdersWithKey;
 
 /*
 The function returns (on the requested day by the requested time):
-- the total number of orders
+- the total number of arrived orders
 - orders per producer
 - orders per healthcare district
+- the total number of arrived vaccines
 */
 
 // endTS is the datetime requested by the user and beginTS is the beginning (time is 00:00:00) of that requested day
@@ -14,42 +15,48 @@ const orders = async (beginTS, endTS) => {
   const noOfOrders = await queryOrders(beginTS, endTS);
 
   const districts = ["HYKS","KYS","OYS","TAYS","TYKS"];
-  const producersAndIds = await queryProducersAndIds();
-  const producers = producersAndIds.map(item => item.producer);
+
+  const vaccineRows = await queryVaccineRows();
+  const producers = vaccineRows.map(item => item.producer);
+  const ids = vaccineRows.map(item => item.id);
+  const injections = vaccineRows.map(item => item.injections);
+
+  const ordersByDistrict = [];
+
+  for (let index in districts) {
+    if (noOfOrders === 0) {
+      ordersByDistrict.push(0);
+    } else {
+      ordersByDistrict.push(await queryOrdersWithKey(beginTS, endTS, 'healthCareDistrict', districts[index]));
+    }
+  }
+
+  const ordersByProducer = [];
+
+  for (let index in ids) {
+    if (noOfOrders === 0) {
+      ordersByProducer.push(0);
+    } else {
+      ordersByProducer.push(await queryOrdersWithKey(beginTS, endTS, 'vaccine', ids[index]));
+    }
+  }
+
+  let noOfVaccines = 0;
+
+  if (noOfOrders !== 0) {
+    for (let index in injections) {
+      noOfVaccines += ordersByProducer[index] * injections[index];
+    }
+  }
 
   let data = {
     "orders": noOfOrders,
     "districts": districts,
-    "ordersByDistrict": [],
+    "ordersByDistrict": ordersByDistrict,
     "producers": producers,
-    "ordersByProducer": []
+    "ordersByProducer": ordersByProducer,
+    "vaccines": noOfVaccines
   }
-  const ordersByDistrict = [];
-
-  for (let district in districts) {
-    if (noOfOrders === 0) {
-      ordersByDistrict.push(0);
-    } else {
-      ordersByDistrict.push(await queryOrdersWithKey(beginTS, endTS, 'healthCareDistrict', districts[district]));
-    }
-  }
-  data.ordersByDistrict = ordersByDistrict;
-
-  let vaccineIdMap = new Map();
-
-  for (let item in producersAndIds) {
-    vaccineIdMap.set(producersAndIds[item].producer, producersAndIds[item].id)
-  }
-  const ordersByProducer = [];
-
-  for (let producer in producers) {
-    if (noOfOrders === 0) {
-      ordersByProducer.push(0);
-    } else {
-      ordersByProducer.push(await queryOrdersWithKey(beginTS, endTS, 'vaccine', vaccineIdMap.get(producers[producer])));
-    }
-  }
-  data.ordersByProducer = ordersByProducer;
 
   return data;
 }
