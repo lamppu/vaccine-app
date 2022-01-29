4b4837f6134a292753e9a5e023f77bbf375ecac1
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2022-01-29 23:40:12 +0200

    Modified vaccinations module and added endpoint to router

--- server/api/router.js
+++ server/api/router.js
@@ -2,11 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-//const ordersAndVaccines = require('./orders.js');
-//const vaccinations = require('./vaccinations.js');
-//const vaccines = require('./vaccines.js');
-
 const orders = require('./orders.js');
+const vaccinations = require('./vaccinations.js');
+
 const validateDate = require('./utils/validate_date.js');
 const formatResponse = require('./utils/format_response.js');
 
@@ -20,6 +18,30 @@ router.get('/orders', async (req, res) => {
       const beginTS = dateString.substring(0, 10) + ' 00:00:00.000000';
 
       const data = await orders(beginTS, endTS);
+
+      res.statusCode = 200;
+      res.send(formatResponse(true, data, null));
+    } else {
+      res.statusCode = 400;
+      res.send(formatResponse(false, null, valid.message));
+    }
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 400;
+    res.send(formatResponse(false, null, "Something went wrong. Please try again."));
+  }
+})
+
+router.get('/vaccinations', async (req, res) => {
+  try {
+    const dateString = req.query.date;
+    const valid = validateDate(dateString);
+
+    if(valid.valid) {
+      const endTS = (dateString.replace('T', ' ')).replace('Z', '');
+      const beginTS = dateString.substring(0, 10) + ' 00:00:00.000000';
+
+      const data = await vaccinations(beginTS, endTS);
 
       res.statusCode = 200;
       res.send(formatResponse(true, data, null));
--- server/api/vaccinations.js
+++ server/api/vaccinations.js
@@ -1,33 +1,56 @@
 const queryVaccinationsWithKey = require('./utils').queryVaccinationsWithKey;
 const queryVaccinationsWithOrderKey = require('./utils').queryVaccinationsWithOrderKey;
 const queryVaccinations = require('./utils').queryVaccinations;
+const queryProducersAndIds = require('./utils').queryProducersAndIds;
 
 /*
-The function returns (on the requested day by the requested time):
+This module returns the following data (on the requested day by the requested time):
 - the total number of vaccinations
 - vaccinations per producer
 - vaccinations per healthcare district
 - the gender distribution of the vaccinations
 */
 
+const getVaccinationsList = async (iteratedList, vaccsNo, key, beginTS, endTS, queryFunction) => {
+  const array = [];
+
+  for (let index in iteratedList) {
+    if (vaccsNo === 0) {
+      array.push(0);
+    } else {
+      if (queryFunction === 'WithOrderKey') {
+        array.push(await queryVaccinationsWithOrderKey(beginTS, endTS, key, iteratedList[index]));
+      } else if (queryFunction === 'WithKey') {
+        array.push(await queryVaccinationsWithKey(beginTS, endTS, key, iteratedList[index]));
+      }
+
+    }
+  }
+  return array;
+}
+
 // endTS is the datetime requested by the user and beginTS is the beginning (time is 00:00:00) of that requested day
 const vaccinations = async (beginTS, endTS) => {
 
-  const numVaccs = await queryVaccinations(beginTS, endTS);
+  const noOfVaccs = await queryVaccinations(beginTS, endTS);
+  const districts = ["HYKS","KYS","OYS","TAYS","TYKS"];
+  const genders = ['female', 'male', 'nonbinary'];
+  const producersAndIds = await queryProducersAndIds();
+  const producers = producersAndIds.map(item => item.producer);
+  const ids = producersAndIds.map(item => item.id);
+
+  const vaccinationsByDistrict = await getVaccinationsList(districts, noOfVaccs, 'healthCareDistrict', beginTS, endTS, 'WithOrderKey');
+  const vaccinationsByGender = await getVaccinationsList(genders, noOfVaccs, 'gender', beginTS, endTS, 'WithKey');
+  const vaccinationsByProducers = await getVaccinationsList(ids, noOfVaccs, 'vaccine', beginTS, endTS, 'WithOrderKey');
 
   let data = {
-    "vaccinations": numVaccs,
-    "zerpfyVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'vaccine', 'Zerpfy'),
-    "antiquaVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'vaccine', 'Antiqua'),
-    "solarBuddhicaVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'vaccine', 'SolarBuddhica'),
-    "hyksVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'healthCareDistrict', 'HYKS'),
-    "kysVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'healthCareDistrict', 'KYS'),
-    "oysVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'healthCareDistrict', 'OYS'),
-    "taysVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'healthCareDistrict', 'TAYS'),
-    "tyksVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithOrderKey(beginTS, endTS, 'healthCareDistrict', 'TYKS'),
-    "femaleVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithKey(beginTS, endTS, 'gender', 'female'),
-    "maleVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithKey(beginTS, endTS, 'gender', 'male'),
-    "nonbinaryVaccinations": (numVaccs === 0) ? 0 : await queryVaccinationsWithKey(beginTS, endTS, 'gender', 'nonbinary')
+    "vaccinations": noOfVaccs,
+    "districts": districts,
+    "vaccinationsByDistrict": vaccinationsByDistrict,
+    "genders": genders,
+    "vaccinationsByGender": vaccinationsByGender,
+    "producers": producers,
+    "vaccinationsByProducers": vaccinationsByProducers
   };
   return data;
 };
