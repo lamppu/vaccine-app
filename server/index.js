c5bc4e35ef4a4108a9b59231bd4e00b9665d4c23
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2022-02-05 22:02:43 +0200

    Added serving app from client/build for production

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+}
 
 const router = require('./api/router');
 
