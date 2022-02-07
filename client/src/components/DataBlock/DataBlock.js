6972d28fe7217972c12b2a93ed759c127ff5e717
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2022-02-07 23:35:25 +0200

    Added isFetching state variable so DataBlock returns loader until the fetch is complete

--- client/src/components/DataBlock/DataBlock.js
+++ client/src/components/DataBlock/DataBlock.js
@@ -4,6 +4,7 @@ import { Audio } from 'react-loader-spinner';
 
 const DataBlock = ({iso, error, onErrorChange, endpoint, getContents, classNm}) => {
   const [data, setData] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,25 +14,32 @@ const DataBlock = ({iso, error, onErrorChange, endpoint, getContents, classNm}) 
         result = await result.json();
         if (result) {
           setData(result.data);
-          onErrorChange({error: !result.success, msg: result.error})
+          onErrorChange({error: !result.success, msg: result.error});
+          setIsFetching(false);
         }
       } catch (e) {
           onErrorChange({error: true, msg: 'Unable to connect to server'})
       }
     }
+
+    setIsFetching(true);
     const validation = validateIso(iso);
     if (validation.valid) {
       fetchData();
     } else {
       onErrorChange({error: true, msg: validation.msg});
     }
+    return () => setIsFetching(false);
   }, [iso, endpoint, onErrorChange])
 
   let contents;
-  if(data && typeof getContents === 'function') {
+
+  if(isFetching) {
+    contents = <Audio color="grey" height={50} width={50} />;
+  } else if(data && typeof getContents === 'function') {
     contents = getContents(data);
   } else {
-    contents = <Audio color="grey" height={50} width={50} />;
+    contents = null;
   }
 
   return (
